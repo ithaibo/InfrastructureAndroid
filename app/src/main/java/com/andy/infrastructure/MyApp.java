54bfb393c3299c32d9cf0e6eb6e295befb51ec07
AndyWu <andywu@gidoor.com>
2017-01-10 15:07:37 +0800

    clear bugly hot fix

--- app/src/main/java/com/andy/infrastructure/MyApp.java
+++ app/src/main/java/com/andy/infrastructure/MyApp.java
@@ -1,22 +1,18 @@
 package com.andy.infrastructure;
 
 import android.app.Application;
+import android.os.Messenger;
 import android.support.multidex.MultiDex;
-
-import com.tencent.tinker.loader.app.TinkerApplication;
-import com.tencent.tinker.loader.shareutil.ShareConstants;
 
 /**
  * Created by Andy on 2017/1/4.
  */
 
-public class MyApp extends TinkerApplication {
-    public MyApp() {
-        super(ShareConstants.TINKER_ENABLE_ALL,
-                "com.andy.infrastructure.MyApplicationLike",
-                "com.tencent.tinker.loader.TinkerLoader",
-                false);
+public class MyApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        MultiDex.install(this);
+
     }
-
-
 }
