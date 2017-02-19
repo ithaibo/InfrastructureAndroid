4b72dafc8e91ad7e54fcfc49f925e4f7074529fc
AndyWu <ithaibo@sina.com>
2017-02-19 22:43:46 +0800

    add demo for custom view

---
+++ app/src/main/java/com/andy/infrastructure/demos/custome_view/CircleView.java
@@ -0,0 +1,83 @@
+package com.andy.infrastructure.demos.custome_view;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
+
+import com.andy.infrastructure.R;
+
+/**
+ * Created by Smily on 2017/2/19.
+ */
+
+public class CircleView extends View {
+    private int mColor = Color.RED;
+    private Paint mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+
+    public CircleView(Context context) {
+        super(context);
+        init();
+    }
+
+    public CircleView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public CircleView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+
+        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.CircleView);
+        mColor = a.getColor(R.styleable.CircleView_circle_color, Color.RED);
+        a.recycle();
+
+        init();
+    }
+
+    private void init() {
+        mPaint.setColor(mColor);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+        int widthSpecMode = MeasureSpec.getMode(widthMeasureSpec);
+        int widthSpecSize = MeasureSpec.getSize(widthMeasureSpec);
+        int heightSpecMode = MeasureSpec.getMode(heightMeasureSpec);
+        int heightSpecSize = MeasureSpec.getSize(heightMeasureSpec);
+
+        if (widthSpecMode == MeasureSpec.AT_MOST &&
+                heightSpecMode == MeasureSpec.AT_MOST) {
+            setMeasuredDimension(100, 100);
+        } else if (widthSpecMode == MeasureSpec.AT_MOST) {
+            setMeasuredDimension(100, heightSpecSize);
+        } else if (heightSpecMode ==MeasureSpec.AT_MOST) {
+            setMeasuredDimension(widthSpecSize, 100);
+        }
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        super.onLayout(changed, left, top, right, bottom);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        final int paddingLeft = getPaddingLeft();
+        final int paddingTop = getPaddingTop();
+        final int paddingRight = getPaddingRight();
+        final int paddingBottom = getPaddingBottom();
+
+        int width = getWidth() - paddingLeft - paddingRight;
+        int height = getHeight() - paddingTop - paddingBottom;
+        int radius = Math.min(width, height)/2;
+
+        canvas.drawCircle(paddingLeft + width/2, paddingTop + height/2, radius, mPaint);
+    }
+}
