9338c0086efbb61f79f87e9ca06665992235f838
AndyWu <andywu@gidoor.com>
2017-02-20 14:07:21 +0800

    modify custom view.

--- app/src/main/java/com/andy/infrastructure/demos/custome_view/CircleView.java
+++ app/src/main/java/com/andy/infrastructure/demos/custome_view/CircleView.java
@@ -61,11 +61,6 @@ public class CircleView extends View {
     }
 
     @Override
-    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
-        super.onLayout(changed, left, top, right, bottom);
-    }
-
-    @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
