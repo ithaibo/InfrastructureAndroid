2a7ca5e2c0a6c223ade582b08c11f11d0eba1c6d
ithaibo <ithaibo@sina.com>
2017-07-25 17:20:53 +0800

    refactor demo-map-nav

---
+++ app/src/main/java/com/andy/infrastructure/demos/map/OutSideNavUtils.java
@@ -0,0 +1,159 @@
+package com.andy.infrastructure.demos.map;
+
+import android.content.ActivityNotFoundException;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.text.TextUtils;
+import android.widget.Toast;
+
+
+import com.andy.baselibrary.utils.AppUtils;
+import com.andy.baselibrary.utils.CommonUtils;
+import com.andy.infrastructure.BuildConfig;
+import com.andy.infrastructure.demos.map.latlon.GCJ02BD09Adapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Andy on 2017/7/25.
+ */
+
+public class OutSideNavUtils {
+    private Context mContext;
+    private DirectionBean channelPicked;
+
+    public OutSideNavUtils(Context context) {
+        this.mContext = context;
+
+        List<DirectionBean> navDataList = new ArrayList<>();
+        DirectionBean amapData = new DirectionBean("com.autonavi.minimap", 0);
+        DirectionBean bdmapData = new DirectionBean("com.baidu.BaiduMap", 1);
+        navDataList.add(amapData);
+        navDataList.add(bdmapData);
+
+        checkChannelAppInstalled(navDataList);
+        pickOneNavChannel(navDataList);
+    }
+
+    private void checkChannelAppInstalled(List<DirectionBean> navDataList) {
+        if (CommonUtils.isListEmpty(navDataList)){
+            return;
+        }
+
+        for (DirectionBean direction : navDataList) {
+            try {
+                PackageInfo packageInfo = mContext.getPackageManager().getPackageInfo(direction.getPackageData().getPackageName(),
+                        PackageManager.GET_ACTIVITIES);
+                if (packageInfo!=null) {
+                    direction.getPackageData().setInstalled(AppUtils.isPackageInstalled(direction.getPackageData().getPackageName(), mContext));
+                    direction.getPackageData().setLabel(mContext.getPackageManager()
+                            .getApplicationLabel(mContext.getPackageManager()
+                                    .getApplicationInfo(direction.getPackageData().getPackageName(), 0))
+                            .toString());
+                }
+            } catch (PackageManager.NameNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void pickOneNavChannel(List<DirectionBean> channelList) {
+        if (CommonUtils.isListEmpty(channelList)) {
+            return;
+        }
+
+        for (int i=0; i<channelList.size(); i++) {
+            try {
+                if (channelList.get(i).getPackageData().isInstalled()) {
+                    channelPicked = channelList.get(i);
+                    break;
+                }
+            } catch (RuntimeException e) {
+            }
+        }
+    }
+
+    private void toPickedChannelNav(DirectionBean navDataList) {
+        Intent intent = null;
+        switch (navDataList.getPriority()) {
+            case 0:
+                intent = new Intent();
+                intent.addCategory("android.intent.category.DEFAULT");
+                intent.setAction("android.intent.action.VIEW");
+                intent.setData(android.net.Uri.parse(
+                        new MapDataBuilder("amapuri://route/plan/")
+                                .addParamter("sourceApplication", BuildConfig.APPLICATION_ID)
+                                .addParamter("dname", navDataList.getdAddress())
+                                .addParamter("dlon", navDataList.getdPoint().getLongitude()+"")
+                                .addParamter("dlat", navDataList.getdPoint().getLatitude()+"")
+                                .addParamter("t", 3+"")     //默认 骑行模式
+                                .build()));
+                intent.setPackage(navDataList.getPackageData().getPackageName());
+                break;
+            case 1:
+                intent = new Intent();
+                intent.setAction("android.intent.action.VIEW");
+
+                intent.setData(android.net.Uri.parse(
+                        "baidumap://map/direction?"+
+                                "destination=name:"+
+                                navDataList.getdAddress()+
+                                "|latlng:"+navDataList.getdPoint().getLatitude()+
+                                ","+navDataList.getdPoint().getLongitude()+
+                                "&mode=riding"      //默认 骑行模式
+                ));
+                intent.setPackage(navDataList.getPackageData().getPackageName());
+                break;
+        }
+
+        if (navDataList.getPackageData().isInstalled()) {
+            if (intent !=null) {
+                try {
+                    mContext.startActivity(intent);
+                } catch (ActivityNotFoundException e) {
+                    e.printStackTrace();
+                }
+            }
+        } else {
+            Toast.makeText(mContext, "请先安装" + navDataList.getPackageData().getLabel() + "APP", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void onNavOutSide(Double destinationLongitude, Double destinationLatitude, String destinationAddress) {
+        if (channelPicked==null) {
+            Toast.makeText(mContext, "请先安装高德地图或百度地图APP", Toast.LENGTH_LONG).show();
+            return;
+        }
+        if (TextUtils.equals(channelPicked.getPackageData().getPackageName(), "com.baidu.BaiduMap")) {
+            double[] converted = GCJ02BD09Adapter.GCJ02ToBD09(destinationLongitude, destinationLatitude);
+            channelPicked.getdPoint().setLongitude(converted[0]);
+            channelPicked.getdPoint().setLatitude(converted[1]);
+        } else {
+            channelPicked.getdPoint().setLongitude(destinationLongitude);
+            channelPicked.getdPoint().setLatitude(destinationLatitude);
+        }
+        channelPicked.setdAddress(destinationAddress);
+        toPickedChannelNav(channelPicked);
+    }
+
+    public void onNavOutSide(Double destinationLongitude, Double destinationLatitude, String destinationAddress,
+                             Double originLongitude, Double originLatitude, String originAddress) {
+        if (channelPicked==null) {
+            Toast.makeText(mContext, "请先安装高德地图或百度地图APP", Toast.LENGTH_LONG).show();
+            return;
+        }
+        if (TextUtils.equals(channelPicked.getPackageData().getPackageName(), "com.baidu.BaiduMap")) {
+            double[] converted = GCJ02BD09Adapter.GCJ02ToBD09(destinationLongitude, destinationLatitude);
+            channelPicked.getdPoint().setLongitude(converted[0]);
+            channelPicked.getdPoint().setLatitude(converted[1]);
+        } else {
+            channelPicked.getdPoint().setLongitude(destinationLongitude);
+            channelPicked.getdPoint().setLatitude(destinationLatitude);
+        }
+        channelPicked.setdAddress(destinationAddress);
+        toPickedChannelNav(channelPicked);
+    }
+}
--- app/src/main/java/com/andy/infrastructure/demos/map/ThirdMapDirection.java
+++ app/src/main/java/com/andy/infrastructure/demos/map/ThirdMapDirection.java
@@ -1,22 +1,8 @@
 package com.andy.infrastructure.demos.map;
 
-import android.content.ActivityNotFoundException;
-import android.content.Intent;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
-import android.widget.Toast;
-
 import com.andy.baselibrary.activity.DataBindActivity;
-import com.andy.baselibrary.utils.AppUtils;
-import com.andy.baselibrary.utils.CommonUtils;
-import com.andy.infrastructure.BuildConfig;
 import com.andy.infrastructure.R;
 import com.andy.infrastructure.databinding.MapDirectionBinding;
-import com.andy.infrastructure.demos.map.latlon.GCJ02BD09Adapter;
-import com.andy.infrastructure.demos.map.latlon.LatLon;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by Andy on 2017/7/24.
@@ -30,113 +16,19 @@ public class ThirdMapDirection extends DataBindActivity<MapDirectionBinding> {
     }
 
     @Override
-    protected void initData() {
-        List<DirectionBean> directionList = new ArrayList<>();
-
-        DirectionBean amapDirection = new DirectionBean("com.autonavi.minimap", 0);
-        amapDirection.setdAddress("肇家浜路地铁站");
-        amapDirection.setdPoint(new LatLon(31.199436, 121.450212, LatLon.PointType.CODE_GCJ02));
-
-        DirectionBean dbMapDirection = new DirectionBean("com.baidu.BaiduMap", 1);
-        dbMapDirection.setdAddress("肇家浜路地铁站");
-        double[] lonLat = GCJ02BD09Adapter.GCJ02ToBD09(121.450212, 31.199436);
-        dbMapDirection.setdPoint(new LatLon(lonLat[0], lonLat[1], LatLon.PointType.CODE_BD09));
-
-        directionList.add(amapDirection);
-        directionList.add(dbMapDirection);
-
-        checkMapPackageInstalled(directionList);
-        pickOneNavChannel(directionList);
-    }
-
-    private void checkMapPackageInstalled(List<DirectionBean> directionList) {
-        if (CommonUtils.isListEmpty(directionList)){
-            return;
-        }
-
-        for (DirectionBean direction : directionList) {
-            try {
-                PackageInfo packageInfo = getPackageManager().getPackageInfo(direction.getPackageData().getPackageName(),
-                        PackageManager.GET_ACTIVITIES);
-                if (packageInfo!=null) {
-                    direction.getPackageData().setInstalled(AppUtils.isPackageInstalled(direction.getPackageData().getPackageName(), ThirdMapDirection.this));
-                    direction.getPackageData().setLabel(getPackageManager()
-                            .getApplicationLabel(getPackageManager()
-                                    .getApplicationInfo(direction.getPackageData().getPackageName(), 0))
-                            .toString());
-                }
-            } catch (PackageManager.NameNotFoundException e) {
-                e.printStackTrace();
-            }
-        }
-    }
+    protected void initData() { }
 
     @Override
     protected void initViews() {
         mDataBind.setClickAction(this.simpleAction);
     }
 
-    private void pickOneNavChannel(final List<DirectionBean> directionList) {
-        for (int i=0; i<directionList.size(); i++) {
-            if (directionList.get(i)!=null && directionList.get(i).getPackageData()!=null && !directionList.get(i).getPackageData().isInstalled()){
-                mDataBind.setDirectionData(directionList.get(i));
-                break;
-            }
-        }
-    }
-
     private SimpleAction simpleAction = new SimpleAction() {
         @Override
         public void action(final DirectionBean direction) {
-            navOutSide(direction);
+            new OutSideNavUtils(mContext).onNavOutSide(121.450212, 31.199436, "肇家浜路地铁站");
         }
     };
-
-    private void navOutSide(DirectionBean direction) {
-        Intent intent = null;
-        switch (direction.getPriority()) {
-            case 0:
-                intent = new Intent();
-                intent.addCategory("android.intent.category.DEFAULT");
-                intent.setAction("android.intent.action.VIEW");
-                intent.setData(android.net.Uri.parse(
-                        new MapDataBuilder("amapuri://route/plan/")
-                                .addParamter("sourceApplication", BuildConfig.APPLICATION_ID)
-                                .addParamter("dname", direction.getdAddress())
-                                .addParamter("dlon", direction.getdPoint().getLongitude()+"")
-                                .addParamter("dlat", direction.getdPoint().getLatitude()+"")
-                                .addParamter("t", 3+"")
-                                .build()));
-                intent.setPackage(direction.getPackageData().getPackageName());
-                break;
-            case 1:
-                intent = new Intent();
-                intent.setAction("android.intent.action.VIEW");
-
-                intent.setData(android.net.Uri.parse(
-                        "baidumap://map/direction?"+
-                                "destination=name:"+
-                                direction.getdAddress()+
-                                "|latlng:"+direction.getdPoint().getLatitude()+
-                                ","+direction.getdPoint().getLongitude()+
-                                "&mode=riding"
-                        ));
-                intent.setPackage(direction.getPackageData().getPackageName());
-                break;
-        }
-
-        if (direction.getPackageData().isInstalled()) {
-            if (intent !=null) {
-                try {
-                    startActivity(intent);
-                } catch (ActivityNotFoundException e) {
-                    e.printStackTrace();
-                }
-            }
-        } else {
-            Toast.makeText(ThirdMapDirection.this, "请先安装" + direction.getPackageData().getLabel() + "APP", Toast.LENGTH_LONG).show();
-        }
-    }
 
     public interface SimpleAction{
         void action(DirectionBean direction);
